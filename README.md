aa19b9279986a6f84dd1ea81e896486c517c08c0
Swatee Swagatika <swaroopkumarhansha@gmail.com>
2023-10-28 17:15:21 +0530

    updates

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-"# website1" 
+https://nevadaepro.com/bso/view/search/external/advancedSearchBid.xhtml?openBids=true
+
+This scraper can be executed by running the file 'nevadaepro.py'
+
+* It extracts the following fileds.
+    o	Bid Solicitation # 
+    o	Buyer
+    o	Description
+    o	Bid Opening Date
+
+    sample data
+    {
+            "Bid Solicitation #": "80DOT-S2558",
+            "Buyer": "Joel Smedes",
+            "Description": "EZ Series 1500 Melter",
+            "Bid Opening Date": "11/17/2023 14:00:00"
+}
+
+*   The extracted json stored in the dir Pages as page numbers i json format.
+    all header information extracted by individual bid soliciation number stored in dir Bid_numbers in respective dir named by the bi numbers
+
+    sample data
+    {
+        "Bid Number:": "01GO-S2521",
+        "Description:": "Independent Verification and Validation (IV&V) Services",
+        "Bid Opening Date:": "11/01/2023 02:00:00 PM",
+        "Purchaser:": "Joel Smedes",
+        "Organization:": "Governor's Office",
+        "Department:": "015 - Governor's Finance Office",
+        "Location:": "1325 - Office of Project Management",
+        "Fiscal Year:": "24",
+        "Type Code:": "",
+        "Allow Electronic Quote:": "Yes",
+        "Alternate Id:": "",
+        "Required Date:": "",
+        "Available Date:": "10/03/2023 03:25:57 PM",
+        "Info Contact:": "Joel Smedes; 775-684-0172; j.smedes@admin.nv.gov",
+        "Bid Type:": "OPEN",
+        "Informal Bid Flag:": "Open Market",
+        "Purchase Method:": "This is a formal RFP request seeking qualified vendors to provide a comprehensive evaluation of the CORE.NV project implementation for the Governor's Finance Office, Office of Project Management . Please see the RFP and the Scope of Work in the 'Attachments' tab for specification information.",
+        "Pre Bid Conference:": "Office of Project Management3850 Arrowhead DriveGovernor's Finance OfficeOffice of the GovernorState of NevadaCarson City,NV 89706USEmail: ASDAPGroup@admin.nv.govPhone: (775)687-7220Alt. Reference: 070",
+        "Bulletin Desc:": "Bill To: Administrative Services Division209 E Musser St Rm 304Administrative Services DivisionDepartment of AdministrationState of NevadaCarson City,NV 89701USEmail: ASDAPGroup@admin.nv.govPhone: (775)684-0273Alt. Reference: 002",
+        "Ship-to Address:": "",
+        "Bill-to Address:": "1"
+}
+
+ * By navigating to the individual bid soliciation number urls, it downloads the attachments stored in the respective folder of the bid number.
+
+* Pagination is set to False. pagn param is set to False by default, it can be changed to True to scrape pagination.
+
+
+
+
--- nevadaepro.py
+++ nevadaepro.py
@@ -2,7 +2,6 @@
 import requests
 from bs4 import BeautifulSoup
 from utilities import *
-import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
@@ -24,44 +23,62 @@ class NevadaPro:
     async def main_page(self,pagn=False):
             
             url = 'https://nevadaepro.com/bso/view/search/external/advancedSearchBid.xhtml?openBids=true'
-            data = requests.get(url) 
-            soup = BeautifulSoup(data.content)
-            facet = soup.find('input',attrs={'name':'javax.faces.ViewState'})['value']
-            content = soup.find('div',attrs={'id':'advSearchResults'})
+            data = requests.get(url)
+            try: 
+                soup = BeautifulSoup(data.content)
+            except:
+                soup = ''
+            try:
+                facet = soup.find('input',attrs={'name':'javax.faces.ViewState'})['value']
+            except:
+                facet = ''
+            try:
+                content = soup.find('div',attrs={'id':'advSearchResults'})
+            except:
+                content = ''
             try:
                 self.max_rows = int(content.find('span',attrs={'class':'ui-paginator-current'}).text.split('of')[-1].strip())
             except:
-                self.max_rows = 100
-            table = content.find('table').find('tbody')
-            thead = content.find('table').find('thead')
+                self.max_rows = 0
+            try:
+                table = content.find('table').find('tbody')
+            except:
+                table = ''
+            try:
+                thead = content.find('table').find('thead')
+            except:
+                thead = ''
             self.rem_list = ['Alternate Id','Contract/Blanket #','Bid Holder List','Awarded Vendor(s)']
             self.heads = []
-            for i in thead.findAll('th'):
-                self.heads.append(i.find('span',attrs={'class':'ui-column-title'}).text)
+            if thead != '':
+                for i in thead.findAll('th'):
+                    self.heads.append(i.find('span',attrs={'class':'ui-column-title'}).text)
             tasks = []
             body = []
-            for i in table.findAll('tr'):
-                for j in i.findAll('td'):
-                    collection = dict()
-                    if i.findAll('td').index(j) == 0:
-                        ind_url = self.domain+j.find('a')['href']
-                        task1 = asyncio.create_task(self.individual_url(ind_url))
-                        tasks.append(task1)
-                        task2 = asyncio.create_task(self.file_downloader(ind_url))
-                        tasks.append(task2)
-                    body.append(j.text)
-                collection = {k:v for k,v in zip(self.heads,body)}
-                removed = [collection.pop(key) for key in self.rem_list]
-                self.main_content.append( collection)
-                body.clear()
-            await asyncio.gather(*tasks)
-            page =1
-            filepath = 'Pages/page{}.json'.format(page)
-            write_json(self.main_content,filepath)
-            self.main_content.clear()
+            if table !='':
+                for i in table.findAll('tr'):
+                    for j in i.findAll('td'):
+                        collection = dict()
+                        if i.findAll('td').index(j) == 0:
+                            ind_url = self.domain+j.find('a')['href']
+                            task1 = asyncio.create_task(self.individual_url(ind_url))
+                            tasks.append(task1)
+                            # task2 = asyncio.create_task(self.file_downloader(ind_url))
+                            # tasks.append(task2)
+                        body.append(j.text)
+                    collection = {k:v for k,v in zip(self.heads,body)}
+                    removed = [collection.pop(key) for key in self.rem_list]
+                    self.main_content.append( collection)
+                    body.clear()
+                await asyncio.gather(*tasks)
+                page =1
+                filepath = 'Pages/page{}.json'.format(page)
+                write_json(self.main_content,filepath)
+                self.main_content.clear()
             
             self.cookies = dict(data.cookies)
-            self.payload['javax.faces.ViewState'] = facet
+            if facet != '':
+                self.payload['javax.faces.ViewState'] = facet
             self.payload['_csrf'] = self.cookies['XSRF-TOKEN']
             if pagn:
                 page += 1
@@ -74,36 +91,45 @@ class NevadaPro:
             count = 0
             collection =dict()
             data = requests.post(self.main_url,headers = self.headers,cookies=self.cookies,data=self.payload)
-            soup = BeautifulSoup(data.content)
-            tags = soup.findAll('td')
-
-            while count < len(tags):
-                body = [i for i in soup.findAll('td')[count:count+10]]
-                body_text = [i.text for i in body]
-                href = body[0].find('a')['href']
-                ind_url = self.domain+ href
-                task1 = asyncio.create_task(self.individual_url(ind_url))
-                tasks.append(task1)
-                task2 = asyncio.create_task(self.file_downloader(ind_url))
-                tasks.append(task2)
-                collection = {k:v for k,v in zip(self.heads,body_text)}
-                removed = [collection.pop(key) for key in self.rem_list]
-                self.main_content.append( collection)
-                count += 10
-            await asyncio.gather(*tasks)
-            filepath = 'Pages/page{}.json'.format(page)
-            write_json(self.main_content,filepath)
-            page += 1
-            self.main_content.clear()
-            rows += 25
-            self.payload['bidSearchResultsForm:bidResultId_first'] = str(rows)            
+            try:
+                soup = BeautifulSoup(data.content)
+                tags = soup.findAll('td')
+            except:
+                tags = []
+            if tags:
+                while count < len(tags):
+                    body = [i for i in soup.findAll('td')[count:count+10]]
+                    body_text = [i.text for i in body]
+                    href = body[0].find('a')['href']
+                    ind_url = self.domain+ href
+                    task1 = asyncio.create_task(self.individual_url(ind_url))
+                    tasks.append(task1)
+                    task2 = asyncio.create_task(self.file_downloader(ind_url))
+                    tasks.append(task2)
+                    collection = {k:v for k,v in zip(self.heads,body_text)}
+                    removed = [collection.pop(key) for key in self.rem_list]
+                    self.main_content.append( collection)
+                    count += 10
+                await asyncio.gather(*tasks)
+                filepath = 'Pages/page{}.json'.format(page)
+                write_json(self.main_content,filepath)
+                page += 1
+                self.main_content.clear()
+                rows += 25
+                self.payload['bidSearchResultsForm:bidResultId_first'] = str(rows)            
               
     async def individual_url(self,url):
 
         data = requests.get(url)
         soup = BeautifulSoup(data.content)
-        title = [i.text.replace('\n','').replace('  ','').strip() for i in soup.findAll('td',attrs={'class':'t-head-01','valign':'top'})]
-        value = [i.text.replace('\n','').replace('  ','').strip() for i in soup.findAll('td',attrs={'class':'tableText-01','valign':'top'})[:title.index('Bill-to Address:')+1]]
+        try:
+            title = [i.text.replace('\n','').replace('  ','').strip() for i in soup.findAll('td',attrs={'class':'t-head-01','valign':'top'})]
+        except:
+            title = []
+        try:
+            value = [i.text.replace('\n','').replace('  ','').strip() for i in soup.findAll('td',attrs={'class':'tableText-01','valign':'top'})[:title.index('Bill-to Address:')+1]]
+        except:
+            value = []
 
         output = {k:v for k,v in zip(title,value)}
         name = output['Bid Number:'].strip()
@@ -118,9 +144,12 @@ class NevadaPro:
         options = webdriver.ChromeOptions()
         options.add_experimental_option('prefs',prefs)
         driver = webdriver.Chrome(options=options)
-        driver.get(f'https://nevadaepro.com/bso/external/bidDetail.sdo?docId={entry}&external=true&parentUrl=close')
-        attachments = driver.find_element(By.XPATH,"//tr/td[contains(text(),'File Attachments:')]/parent::node()/td[2]")
-        files = attachments.find_elements(By.TAG_NAME,'a')
+        try:
+            driver.get(f'https://nevadaepro.com/bso/external/bidDetail.sdo?docId={entry}&external=true&parentUrl=close')
+            attachments = driver.find_element(By.XPATH,"//tr/td[contains(text(),'File Attachments:')]/parent::node()/td[2]")
+            files = attachments.find_elements(By.TAG_NAME,'a')
+        except:
+            files = []
         if len(files)> 0:
             for i in files:
                 i.click()
@@ -131,6 +160,6 @@ class NevadaPro:
 
 runner = NevadaPro()
 async_runner = asyncio.get_event_loop()
-async_runner.run_until_complete(runner.main_page())
+async_runner.run_until_complete(runner.main_page())#pagn=True(optional)
 # runner.main_page()#pagn=True(optional)
 # runner.file_downloader()
